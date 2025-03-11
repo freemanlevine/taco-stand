02fe039933305b5c44df712ff0e90884fd453a0e
Freeman Levine <freemanelevine@gmail.com>
2025-03-10 22:47:54 -0700

    consolidate migrations
    * constraint naming conventions

--- src/models.py
+++ src/models.py
@@ -1,11 +1,18 @@
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import MetaData
 from sqlalchemy import String, ForeignKey, Integer
 from typing import List
 
 
 class Base(DeclarativeBase):
-    pass
+    metadata = MetaData(naming_convention={
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_name)s",
+        "ck": "ck_%(table_name)s_`%(constraint_name)s`",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s"
+    })
 
 class Shop(Base):
     """A shop that sells goods to customers"""
